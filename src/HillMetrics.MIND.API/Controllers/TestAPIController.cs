9fc7b476cbdc5a69ee8cc1fc099ea068ffab44fc
selvir-osmanovic <selvir_sk@hotmail.com>
2025-02-10 20:16:56 +0100

    HIL-179

--- src/HillMetrics.MIND.API/Controllers/TestAPIController.cs
+++ src/HillMetrics.MIND.API/Controllers/TestAPIController.cs
@@ -2,15 +2,18 @@
 using HillMetrics.MIND.API.Contracts.Responses.Tests;
 using HillMetrics.MIND.API.Endpoints;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.RateLimiting;
 
 namespace HillMetrics.MIND.API.Controllers;
 
 [Route("api/v{v:apiVersion}")]
-[EnableRateLimiting("allow5000requestsPerSecond_fixed")]
+[AllowAnonymous]
+//[EnableRateLimiting("allow5000requestsPerSecond_fixed")]
 public class TestAPIController(IMediator mediator) : BaseHillMetricsController(mediator)
 {
+    [AllowAnonymous]
     [DisableRateLimiting]
     [HttpGet(InternalRoutes.Test.Get)]
     public async Task<ActionResult<TestResponse>> GetAsync()
@@ -18,14 +21,15 @@ public class TestAPIController(IMediator mediator) : BaseHillMetricsController(m
         return new TestResponse(new TestValue() { TestString = "Hello World", DateTime = DateTime.Now });
     }
 
+    [AllowAnonymous]
     [HttpGet(InternalRoutes.Test.Error)]
     public async Task<ActionResult<TestResponse>> GetAsyncResultError()
     {
         //in case of failure
-        return new ErrorAPIResult(new Core.API.Exceptions.ApiException(400, "Bad request", "error details example"));
+        return new ErrorApiActionResult(new ErrorApiResponse(new Core.API.Exceptions.ApiException("Bad request"), System.Net.HttpStatusCode.BadRequest));
     }
 
-    [EnableRateLimiting("singleRequestByUser")]
+    //[EnableRateLimiting("singleRequestByUser")]
     [HttpGet("api/test/test123")]
     public async Task<ActionResult<TestResponse>> Test()
     {
