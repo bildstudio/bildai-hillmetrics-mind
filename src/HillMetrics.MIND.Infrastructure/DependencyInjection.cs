ae6bac13af9b70da5c871c0ff9c1e73d40a467c0
selvir-osmanovic <selvir_sk@hotmail.com>
2025-04-07 23:25:20 +0200

    HIL-312

--- src/HillMetrics.MIND.Infrastructure/DependencyInjection.cs
+++ src/HillMetrics.MIND.Infrastructure/DependencyInjection.cs
@@ -9,14 +9,27 @@ using HillMetrics.Normalized.Infrastructure.Database.Repository;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using StackExchange.Redis;
 
 namespace HillMetrics.MIND.Infrastructure
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddMindAuthenticationServices(this IServiceCollection services)
+        public static IServiceCollection AddMindAuthenticationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.TryAddSingleton<IAuthenticationService, AzureAdAuthenticationService>();
+            services.TryAddSingleton<ITokenExchangeService, TokenExchangeService>();
+
+            //inject redis multiplexer as we use it in TokenExchangeService
+            string? redisConnectionString = configuration.GetConnectionString("Redis");
+            if (string.IsNullOrEmpty(redisConnectionString))
+            {
+                string errorMsg = "'Redis' connectionString is missing in ConnectionStrings section";
+                throw new Exception(errorMsg);
+            }
+
+            services.AddSingleton<IConnectionMultiplexer>(s => ConnectionMultiplexer.Connect(redisConnectionString));
+
             services.TryAddSingleton<IRedirectUrlValidator, RedirectUrlValidator>();
 
             return services;
