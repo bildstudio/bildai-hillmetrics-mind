a99b5e885f2cd60c5b46fcdc91c45a340924fa4f
selvir-osmanovic <selvir_sk@hotmail.com>
2025-04-10 01:22:03 +0200

    HIL-313

--- src/HillMetrics.MIND.Infrastructure/Authentication/KeycloakConfigMind.cs
+++ src/HillMetrics.MIND.Infrastructure/Authentication/KeycloakConfigMind.cs
@@ -19,7 +19,7 @@
             if (string.IsNullOrEmpty(Audiences))
                 return [Azure.ClientId];
 
-            return Audiences.Split(";", StringSplitOptions.RemoveEmptyEntries);
+            return Audiences.Split(new char[';'], StringSplitOptions.RemoveEmptyEntries);
         }
     }
 }
